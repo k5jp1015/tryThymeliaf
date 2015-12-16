e85660f9568bbfff28ea7b5b04ab3f68ace145c6
keigo.magami <k.magami@aim-soft.co.jp>
2015-12-16 11:11:30 +0900

    first Commit

---
+++ src/main/java/demo/thymeleaf/WithThymeleafApplication.java
@@ -0,0 +1,12 @@
+package demo.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithThymeleafApplication.class, args);
+    }
+}
---
+++ src/test/java/demo/thymeleaf/WithThymeleafApplicationTests.java
@@ -0,0 +1,18 @@
+package demo.thymeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WithThymeleafApplication.class)
+@WebAppConfiguration
+public class WithThymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
