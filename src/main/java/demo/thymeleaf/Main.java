809a3d93a965923e940a6f6bcad2b8ca23ff0b28
keigo.magami <k.magami@aim-soft.co.jp>
2015-12-16 11:16:15 +0900

    second Commit

--- src/main/java/demo/thymeleaf/Main.java
+++ src/main/java/demo/thymeleaf/Main.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WithThymeleafApplication {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(WithThymeleafApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
