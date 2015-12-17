34e40ea69ce41ae5243b6b964513d72e62037b5c
keigo.magami <k.magami@aim-soft.co.jp>
2015-12-17 10:43:35 +0900

    テンプレートエンジンを用いた埋め込み　id = 10 まで

---
+++ src/main/java/demo/thymeleaf/web/HelloController.java
@@ -0,0 +1,22 @@
+package demo.thymeleaf.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String hello(Model model) {
+        Hoge hoge = new Hoge();
+        hoge.id = 10;
+        hoge.value = "hoge";
+
+        model.addAttribute("myData", hoge);
+
+        return "hello";
+    }
+}
---
+++ src/main/java/demo/thymeleaf/web/Hoge.java
@@ -0,0 +1,12 @@
+package demo.thymeleaf.web;
+
+public class Hoge {
+
+    public int id;
+    public String value;
+
+    @Override
+    public String toString() {
+        return "Hoge [id=" + id + ", value=" + value + "]";
+    }
+}
